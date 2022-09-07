f50dea7e6f0d004b48f9a19a703babf96a40ffd8
Mazen alotaibi <mazen.alotaibi9@gmail.com>
2022-09-07 17:28:25 +0300

    adding mazzen info

---
+++ temp.py
@@ -0,0 +1,18 @@
+def mazenFunc():
+  firstName='mazen'
+  lastName='alotaibi'
+  return firstName+lastName
+#please fill the variables  
+def fahadFunc():
+  firstName=Null
+  lastName=Null
+  return firstName+lastName
+def hatemFunc():
+  firstName=Null
+  lastName=Null
+  return firstName+lastName
+def abdullahFunc():
+  firstName=Null
+  lastName=Null
+  return firstName+lastName
+print(mazenFunc())
